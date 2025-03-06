aa5c5db5619d38eff5473ec2166b3cd7a2f07264
JakubDekar <94068114+JakubDekar@users.noreply.github.com>
2025-03-06 09:37:34 +0100

    comit

--- tests/test_calculator_parametrized.py
+++ tests/test_calculator_parametrized.py
@@ -5,7 +5,7 @@ def test_add():
     assert calculator.add(1, 2) == 3
     assert calculator.add(0, 5) == 5
     assert calculator.add(-2, 9) == 7
-    assert calculator.add(0, 0) == -7
+    assert calculator.add(5, -8) == -3
 
 def test_add_2():  
     assert calculator.add(1, 2) == 3
@@ -14,6 +14,10 @@ def test_add_2():
     assert calculator.add(5, -8) == -3
 
 @pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (0, 5, 5), (-2, 9, 7), (5, -8, -3)],)
-
 def test_add_parametrized(a, b, expected):
     assert calculator.add(a, b) == expected
+
+
+@pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (0, 5, 5), (-2, 9, 7), (5, -8, -3)],)
+def test_add_wrong_parametrized(a,b, expected):
+    assert calculator.add_wrong(a, b) == expected
